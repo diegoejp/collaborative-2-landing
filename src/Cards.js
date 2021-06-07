77c0c600d7785488db38c8e7d4747c52abba1db9
diegoejp <d.jacobpulgar@gmail.com>
2021-06-07 13:21:53 -0400

    agregando

--- src/Cards.js
+++ src/Cards.js
@@ -1,16 +1,16 @@
 
 
 
-const Card = (()=>{
+const Card = (({tittle,text,imgUrl,link,textLink})=>{
     return(
-        <div className="card" style={{
+        <div className="card col-3" style={{
             width: "18rem"
         }}>
-            <img src="..." className="card-img-top" alt="..."/>
+            <img src={imgUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{tittle}</h5>
+                <p className="card-text">{text}</p>
+                <a href={link} className="btn btn-primary">{textLink}</a>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom"
 import Card from "./Cards";
-import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 
 const App = (()=>{
     return(
         <>
         {/* Aca los componentes*/}
         <div className="container">
-
-            <Card/>
+          
+            <div className="row">{/*Este div para almacenar las cartas*/}
+                <Card text = "Esta es la primera carta" tittle="Carta 1" imgUrl="" link="" textLink="Vamos"/>
+                <Card text = "Hola"/>
+                <Card text = "Hola"/>
+                <Card text = "Hola"/>
+            </div>
         </div>
 
         
