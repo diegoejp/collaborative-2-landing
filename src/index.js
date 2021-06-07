500141ec5a80970eb57053d2345e8f65e00b363c
diegoejp <d.jacobpulgar@gmail.com>
2021-06-07 12:15:59 -0400

    agregado el app a index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+const App = (()=>{
+    return(
+        <>
+        {/* Aca los componentes*/}
+        <h1>Probando </h1>
+
+
+        
+        </>
+    )
+});
+
+ReactDOM.render(<App/>,document.querySelector("#root"));
