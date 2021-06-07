0c3b1e5a61a72a5cfc6a269a05e079891f43aa5f
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-06-07 13:13:51 -0400

    jumbotron

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom"
 import 'bootstrap/dist/css/bootstrap.css'
 import Navbar from './navbar.js'
+import Jumbotron from './jumbotron'
 import Card from "./Cards";
 
 const App = (()=>{
     return(
         <>
         {/* Aca los componentes*/}
+        <Navbar/>
         <div className="container">
-            <Navbar/>
+            <Jumbotron/>
             <Card/>
         </div>
 
