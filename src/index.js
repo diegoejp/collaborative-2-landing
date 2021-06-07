6a8122630c10cdb4134b6985323790818fad24d1
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-06-07 13:01:28 -0400

    navbar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom"
 import 'bootstrap/dist/css/bootstrap.css'
+import Navbar from './navbar.js'
 
 const App = (()=>{
     return(
         <>
         {/* Aca los componentes*/}
+        <Navbar/>
         <h1>Probando </h1>
-
+        
 
         
         </>
